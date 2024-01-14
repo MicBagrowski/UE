a82d53abb0510b6dfa4817bfae4fb5cdeeb91bea
student <bagrowski0001@gmail.com>
2024-01-14 13:09:33 +0100

    wykrywanie twarzy - próba z url

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
+import urllib
 from flask import Flask
 from flask_restful import Resource, Api
+
 
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
@@ -21,8 +23,19 @@ class PeopleCounter(Resource):
 
         return {'peopleCount': len(rects)}
 
+    def get(self):
+        url="https://www.shutterstock.com/pl/image-photo/happy-business-colleagues-having-discussion-over-2262759289"
+        image = cv2.imread('ludzie.jpg')
+        image = cv2.resize(image, (700, 400))
+
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+        return {'peopleCount2': len(rects)}
 
 api.add_resource(PeopleCounter, '/')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
